eab78e08ce335458627594cf42e87b23fb8e604c
HowDoIprintHelloWorld <87938212+HowDoIprintHelloWorld@users.noreply.github.com>
2022-10-24 09:40:07 +0200

    Create messages.py

---
+++ src/messages.py
@@ -0,0 +1,8 @@
+verifyChall = """Hi user! Please check the pinned message in this channel for instructions.\nYour verification challenge: """
+
+wrongChall = "❌ Wrong! ❌"
+corrChall = "✅ Correct! Check out the rules to begin! 👋"
+getChall = "🖐 Wait! Use '!verify' to get a challenge!"
+challUsage = "🖐 Wait! Use '!verify (solution)' to verify!"
+
+congratsJoining = "🥳 You are now fully verified. Have fun! ✨"
